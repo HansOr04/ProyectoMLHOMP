02d609bd8d101819b763b936a77b7cc93860cc80
Hans Ortiz <127800719+HansOr04@users.noreply.github.com>
2024-10-14 17:25:08 -0500

    Update IAparmentService.cs

--- Services/Interfaces/IAparmentService.cs
+++ Services/Interfaces/IAparmentService.cs
@@ -1,22 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using ProyectoMLHOMP.Models;
-using ProyectoMLHOMP.ViewModels;
-
 namespace ProyectoMLHOMP.Services.Interfaces
 {
     public interface IApartmentService
     {
-        Task<IEnumerable<ApartmentViewModel>> GetAllApartmentsAsync();
-        Task<ApartmentViewModel> GetApartmentByIdAsync(int id);
-        Task<ApartmentViewModel> CreateApartmentAsync(ApartmentViewModel apartmentViewModel);
-        Task<ApartmentViewModel> UpdateApartmentAsync(int id, ApartmentViewModel apartmentViewModel);
-        Task<bool> DeleteApartmentAsync(int id);
-        Task<IEnumerable<ApartmentViewModel>> SearchApartmentsAsync(string searchTerm, DateTime? checkIn, DateTime? checkOut, decimal? minPrice, decimal? maxPrice);
-        Task<bool> IsApartmentAvailableAsync(int id, DateTime checkIn, DateTime checkOut);
-        Task<IEnumerable<ReviewViewModel>> GetApartmentReviewsAsync(int apartmentId);
-        Task<double> GetApartmentAverageRatingAsync(int apartmentId);
-        Task<IEnumerable<ApartmentViewModel>> GetApartmentsByOwnerIdAsync(string ownerId);
     }
 }
